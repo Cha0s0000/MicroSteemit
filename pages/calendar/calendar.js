6868e42395f5830211c71e830859288698b1229e
Cha0s <457318796@qq.com>
2018-06-01 00:18:57 +0800

    add writing TODO list in calendar
    add writing TODO list in calendar

--- pages/calendar/calendar.js
+++ pages/calendar/calendar.js
@@ -19,7 +19,12 @@ Page({
     },
     swiperMap: ['first', 'second', 'third', 'fourth'],
     swiperIndex: 1,
-    showCaldenlar: false
+    showCaldenlar: false,
+    input: '',
+    todos: [],
+    leftCount: 0,
+    allCompleted: false,
+    logs: []
   },
 
   /**
@@ -365,5 +370,136 @@ Page({
   },
   formatDay(day) {
     return `${(day + '').length > 1 ? '' : '0'}${day}`
-  }
+  },
+	/**
+	 * 
+	 * Get user input in the box 
+	 */
+  inputChangeHandle: function (e) {
+    this.setData({ input: e.detail.value })
+  },
+
+	/**
+	 * 
+	 * Add to TODO list after clicking enter
+	 */
+  addTodoHandle: function (e) {
+    if (!this.data.input || !this.data.input.trim()) return
+    var todos = this.data.todos
+    todos.push({ name: this.data.input, completed: false })
+    var logs = this.data.logs
+    logs.push({ timestamp: new Date(), action: 'Add', name: this.data.input })
+    this.setData({
+      input: '',
+      todos: todos,
+      leftCount: this.data.leftCount + 1,
+      logs: logs
+    })
+    this.save()
+  },
+	/**
+	 * 
+	 * Toggle the specified items in TODO list
+	 */
+  toggleTodoHandle: function (e) {
+    var index = e.currentTarget.dataset.index
+    var todos = this.data.todos
+    todos[index].completed = !todos[index].completed
+    var logs = this.data.logs
+    logs.push({
+      timestamp: new Date(),
+      action: todos[index].completed ? 'Finish' : 'Restart',
+      name: todos[index].name
+    })
+    this.setData({
+      todos: todos,
+      leftCount: this.data.leftCount + (todos[index].completed ? -1 : 1),
+      logs: logs
+    })
+    this.save()
+  },
+	/**
+	 * 
+	 * Remove the specified items from TODO list
+	 */
+  removeTodoHandle: function (e) {
+    var index = e.currentTarget.dataset.index
+    var todos = this.data.todos
+    var remove = todos.splice(index, 1)[0]
+    var logs = this.data.logs
+    logs.push({ timestamp: new Date(), action: 'Remove', name: remove.name })
+    this.setData({
+      todos: todos,
+      leftCount: this.data.leftCount - (remove.completed ? 0 : 1),
+      logs: logs
+    })
+    this.save()
+  },
+  /**
+	 * 
+	 * Toggle all items in TODO list
+	 */
+  toggleAllHandle: function (e) {
+    this.data.allCompleted = !this.data.allCompleted
+    var todos = this.data.todos
+    for (var i = todos.length - 1; i >= 0; i--) {
+      todos[i].completed = this.data.allCompleted
+    }
+    var logs = this.data.logs
+    logs.push({
+      timestamp: new Date(),
+      action: this.data.allCompleted ? 'Finish' : 'Restart',
+      name: 'All todos'
+    })
+    this.setData({
+      todos: todos,
+      leftCount: this.data.allCompleted ? 0 : todos.length,
+      logs: logs
+    })
+    this.save()
+  },
+  /** 
+	 * 
+	 * Clear the completed item 
+	 */
+  clearCompletedHandle: function (e) {
+    var todos = this.data.todos
+    var remains = []
+    for (var i = 0; i < todos.length; i++) {
+      todos[i].completed || remains.push(todos[i])
+    }
+    var logs = this.data.logs
+    logs.push({
+      timestamp: new Date(),
+      action: 'Clear',
+      name: 'Completed todo'
+    })
+    this.setData({ todos: remains, logs: logs })
+    this.save()
+  },
+   /** 
+	 * 
+	 * Svae TODO list data into app storage
+	 */
+  save: function () {
+    wx.setStorageSync('todo_list', this.data.todos)
+    wx.setStorageSync('todo_logs', this.data.logs)
+  },
+  /** 
+	 * 
+	 * load TODO list data from  app storage
+	 */
+  load: function () {
+    var todos = wx.getStorageSync('todo_list')
+    if (todos) {
+      var leftCount = todos.filter(function (item) {
+        return !item.completed
+      }).length
+      this.setData({ todos: todos, leftCount: leftCount })
+    }
+    var logs = wx.getStorageSync('todo_logs')
+    if (logs) {
+      this.setData({ logs: logs })
+    }
+  },
 })
